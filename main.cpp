112a1136772f10291e3c5f76378877a05784f8e0
suhasg1 <suhas.g@s.amity.edu>
2025-06-01 21:50:55 +0530

    simple structure

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    do {
+        cout << "\n--- Bank Management System ---\n";
+        cout << "1. Create Account\n";
+        cout << "2. Deposit\n";
+        cout << "3. Withdraw\n";
+        cout << "4. Check Balance\n";
+        cout << "5. Search Account\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
+}
+
