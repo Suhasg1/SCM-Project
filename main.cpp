ee4736194adf52aacc850ab40a368e5f3a7bc763
suhasg1 <suhas.g@s.amity.edu>
2025-06-01 21:57:41 +0530

    Added new account feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+struct Account {
+    int accNo;
+    string name;
+    double balance;
+};
+
+const int MAX = 100;
+Account accounts[MAX];
+int accountCount = 0;
+
+void createAccount() {
+    Account acc;
+    cout << "Enter Account Number: ";
+    cin >> acc.accNo;
+    cout << "Enter Name: ";
+    cin.ignore();
+    getline(cin, acc.name);
+    cout << "Enter Initial Deposit: ";
+    cin >> acc.balance;
+    accounts[accountCount++] = acc;
+    cout << "Account created successfully!\n";
+}
+
 int main() {
     int choice;
     do {
@@ -13,6 +36,31 @@ int main() {
         cout << "6. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
-    }
+
+        switch (choice) {
+            case 1:
+                createAccount();
+                break;
+            case 2:
+                // deposit();
+                break;
+            case 3:
+                // withdraw();
+                break;
+            case 4:
+                // checkBalance();
+                break;
+            case 5:
+                // searchAccount();
+                break;
+            case 6:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice.\n";
+        }
+    } while (choice != 6);
+
+    return 0;
 }
 
